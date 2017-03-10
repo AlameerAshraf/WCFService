780c33a61b25e26f31744349b0286d8fca0b58f2
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-03-10 12:37:58 +0200

    Service Update and Client
    - Fix The "BAD REQUEST 400" ERROR in the asp.net Service Consumer .
    - Modify The Add Function Behavoiur in the Service Operations .

---
+++ WebApplicationConsumer/ServiceConsumer/ServiceConsumer/HomePage.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="ServiceConsumer.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server" Height="234px" Width="470px"></asp:TextBox>
+    
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplicationConsumer/ServiceConsumer/ServiceConsumer/HomePage.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ServiceConsumer
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //WebClient MyClient = new WebClient();
+            //string Array = MyClient.DownloadString("http://localhost:8258/Magic.svc/Employees");
+            //TextBox1.Text = Array;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            // myclient.Headers.Add("Content-Type", "text");
+            // myclient.Encoding = Encoding.UTF8;
+
+            WebClient myclient = new WebClient();
+            myclient.Headers[HttpRequestHeader.ContentType] = "application/json";
+            EmpInfo ss = new EmpInfo();
+            ss.EmpId = 3;
+            ss.EmpName = "asasa";
+            ss.EmpSalary = 20555;
+            ss.EmpAddress = "asasasas";
+            ss.ProjectName = "sdsds";
+            ss.ProjectId = 5;
+
+        string Obj = JsonConvert.SerializeObject(ss);
+        
+        string replay = myclient.UploadString("http://localhost:8258/Magic.svc/InsertEmployee", "POST", Obj);
+        TextBox1.Text = replay;
+        }
+    }
+
+    public class EmpInfo
+    {
+        public int EmpId { get; set; }
+       
+        public string EmpName { get; set; }
+        
+        public decimal EmpSalary { get; set; }
+    
+        public string EmpAddress { get; set; }
+      
+        public int ProjectId { get; set; }
+       
+        public string ProjectName { get; set; }
+    }
+
+}
