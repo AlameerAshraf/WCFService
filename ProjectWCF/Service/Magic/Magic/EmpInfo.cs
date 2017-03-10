d9f1386e63ae4319d969041b492dcc3c93574471
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-03-10 02:14:27 +0200

    Fix Bug
    Fix "Project NULL Values " Bug .

---
+++ ProjectWCF/Service/Magic/Magic/EmpInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace Magic
+{
+    [DataContract]
+    public class EmpInfo
+    {
+        [DataMember]
+        public int EmpId { get; set; }
+        [DataMember]
+        public string EmpName { get; set; }
+        [DataMember]
+        public decimal EmpSalary { get; set; }
+        [DataMember]
+        public string EmpAddress { get; set; }
+        [DataMember]
+        public int ProjectId { get; set; }
+        [DataMember]
+        public string ProjectName { get; set; }
+
+
+    }
+}
